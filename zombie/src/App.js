e3a77397b246edc488fb8ea3dc08b6460508fe5d
TheHawk4453 Hawk <matthawkins44@gmail.com>
2022-09-30 19:48:43 -0500

    forgot to save file before last commit

--- zombie/src/App.js
+++ zombie/src/App.js
@@ -1,9 +1,12 @@
 import "./App.scss"
+
+
 function App() {
   return (
     <div className="app">
         <h1 className="app__title">Zombie <br></br> Survival<br></br> Guide<br></br></h1>
         <iframe className="app__gif" src="https://giphy.com/embed/y1ewxG8aFWk0M" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/zombie-y1ewxG8aFWk0M"></a></p>
+        
     </div>
   );
 }
