5f6406e4a8aa94467198692eb402291c23ee22c1
Michael Hendrick <silver147995@gmail.com>
2022-10-19 17:21:13 -0500

    added that h1 ayo

--- zombie/src/App.js
+++ zombie/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="app">
         <h1 className="app__title">Zombie <br></br> Survival<br></br> Guide<br></br></h1>
         <iframe className="app__gif" src="https://giphy.com/embed/y1ewxG8aFWk0M" width="480" height="270" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/zombie-y1ewxG8aFWk0M"></a></p>
-        
+        <h1>akladshlkj;asdf</h1>
     </div>
   );
 }
